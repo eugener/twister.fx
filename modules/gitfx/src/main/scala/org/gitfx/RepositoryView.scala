cbf57c0594ba8c811f28b393942efc1dda13982a
Eugene Ryzhikov <eryzhikov@gmail.com>
2017-06-02 23:53:55 -0400

    Enhacements to GitFX UI

--- modules/gitfx/src/main/scala/org/gitfx/RepositoryView.scala
+++ modules/gitfx/src/main/scala/org/gitfx/RepositoryView.scala
@@ -1,11 +1,21 @@
 package org.gitfx
 
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import javafx.beans.property.{ObjectProperty, SimpleObjectProperty}
+import javafx.fxml.FXML
+import javafx.geometry.Pos
+import javafx.scene.control.{Label, ListCell, ListView}
+import javafx.scene.layout.{GridPane, HBox, Priority, VBox}
 
+import org.eclipse.jgit.revwalk.RevCommit
 import org.gitfx.domain.Repository
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 import org.twisterfx.FXMLView
+import org.eclipse.jgit.revwalk.{RevCommit => JGitCommit}
+
+import scala.collection.JavaConverters._
 
 @Component
 @Scope("prototype")
@@ -16,5 +26,72 @@ class RepositoryViewController {
   val repositoryProperty: ObjectProperty[Repository] = new SimpleObjectProperty[Repository]()
   def repository: Repository = repositoryProperty.get
   def repository_=( value: Repository ): Unit = repositoryProperty.set(value)
+
+  @FXML
+  var commitList: ListView[RevCommit] = _
+
+  def initialize(): Unit = {
+
+      commitList.setCellFactory( _ => new CommitCell() )
+
+      repositoryProperty.addListener((_,_,repo) => refreshRepo() )
+  }
+
+  def refreshRepo(): Unit = {
+     commitList.getItems.setAll(repository.getCommits.asJavaCollection)
+  }
+
+}
+
+private class CommitCell extends ListCell[JGitCommit] {
+
+  private val pane = new CommitPanel
+  setGraphic(pane)
+  setText(null)
+
+
+  override def updateItem(item: JGitCommit, empty: Boolean): Unit = {
+    super.updateItem(item, empty)
+    pane.commit = if (empty) null else item
+  }
+}
+
+private class CommitPanel extends VBox {
+
+  val format = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+
+  val committerName = new Label()
+  val date          = new Label()
+  val id            = new Label()
+  val comment       = new Label()
+
+  val firstRow = new HBox(10, committerName, date)
+  HBox.setHgrow(committerName, Priority.ALWAYS)
+  firstRow.setAlignment(Pos.CENTER_RIGHT)
+  val secondRow = new HBox(10, id, comment)
+  HBox.setHgrow(comment, Priority.ALWAYS)
+  getChildren.addAll(firstRow,secondRow)
+
+  setMaxWidth(250)
+
+  private var commitOption: Option[JGitCommit] = None
+  def commit: JGitCommit = commitOption.get
+
+  def commit_=(value: JGitCommit): Unit = {
+    commitOption = Option(value)
+
+    committerName.setText(commitOption.map(_.getAuthorIdent.getName).orNull)
+    id.setText(commitOption.map(_.getName.take(8)).orNull)
+    comment.setText(commitOption.map(_.getShortMessage).orNull)
+    date.setText(commitOption.map { c =>
+      val date = c.getAuthorIdent.getWhen.toInstant.atZone(ZoneId.systemDefault).toLocalDate
+      date.format(format)
+    }.orNull)
+  }
+
+
+
+
 
 }
--- modules/gitfx/src/main/scala/org/gitfx/domain/Repository.scala
+++ modules/gitfx/src/main/scala/org/gitfx/domain/Repository.scala
@@ -1,32 +1,46 @@
 package org.gitfx.domain
 
 import java.io.File
-import java.nio.file.Path
+import java.nio.file.{Files, Path, Paths}
 
+import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.lib.{Repository => JGitRepo}
+import org.eclipse.jgit.revwalk.{RevCommit => JGitCommit}
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
 import scala.util.Try
+import scala.collection.JavaConverters._
 
 object Repository {
 
+   private[domain] val GIT_DIR = ".git"
    def open( location: Path ): Try[Repository] = Try(new Repository(location))
 
 }
 
-case class Repository( location: Path ) {
+case class Repository( val location: Path ) {
+
+   import Repository._
+
+   private val gitDir = if ( location.endsWith(GIT_DIR) ) location else Paths.get(location.toString).resolve(GIT_DIR)
+   require( Files.exists(gitDir))
+
+   private val repository: JGitRepo = {
+      new FileRepositoryBuilder()
+        .setGitDir(gitDir.toFile)
+        .readEnvironment // scan environment GIT_* variables
+        .build
+   }
+
+   private lazy val git = new Git(repository)
 
    lazy val name: String =  {
       val nameCount = location.getNameCount
       if ( nameCount> 0 ) location.getName(nameCount-1).toString else "???"
    }
 
-   val repository: JGitRepo = {
-      new FileRepositoryBuilder()
-        .setGitDir(location.toFile)
-        .readEnvironment // scan environment GIT_* variables
-        .findGitDir // scan up the file system tree
-        .build
-   }
+   def getCommits: List[JGitCommit] = git.log.all.call.asScala.toList
+
+
 
 }
