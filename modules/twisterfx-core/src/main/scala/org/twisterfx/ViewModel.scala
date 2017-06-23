b3b827063cbde759523b44371db20128bcfebde3
Eugene Ryzhikov <eryzhikov@gmail.com>
2017-06-23 16:28:33 -0400

    Enhancements to the ViewModel API and implementation

--- modules/twisterfx-core/src/main/scala/org/twisterfx/ViewModel.scala
+++ modules/twisterfx-core/src/main/scala/org/twisterfx/ViewModel.scala
@@ -12,80 +12,125 @@ object ViewModel {
     type FXProperty[T] = Property[T] with WritableObjectValue[T]
 }
 
-import ViewModel._
+import org.twisterfx.ViewModel._
 
 abstract class ViewModel[SUBJECT]( val subject: SUBJECT ) {
 
     private val knownProperties = mutable.Set[Binding[_]]()
 
-    // Creates property bound to the source property and registers binding in the view model
-    final def bind[T](sourceProperty: FXProperty[T]): FXProperty[T] = bind(FXPropertyBinding[T](sourceProperty))
+    /**
+      * Creates property bound to the source property and registers binding in the view model
+      * @param sourceProperty source property
+      * @tparam T property type
+      * @return newly created property bound to the source property
+      */
+    final def bind[T: TypeTag](sourceProperty: FXProperty[T]): FXProperty[T] = bind(new FXPropertyBinding[T](sourceProperty))
 
-    // Creates property bound to a bean property  and registers it in view model
-    final def bind[T: TypeTag](bean: AnyRef, propertyName: String ): FXProperty[T] = bind(BeanPropertyBinding[T](bean, propertyName))
+    /**
+      * Creates property bound to a bean property and registers it in view model
+      * @param bean
+      * @param propertyName
+      * @tparam T property type
+      * @return newly created property bound to the bean property
+      */
+    final def bind[T: TypeTag](bean: AnyRef, propertyName: String ): FXProperty[T] = bind( new BeanPropertyBinding[T](bean, propertyName))
+
+    /**
+      * Creates property bound to provided getter and setter
+      * @param getter
+      * @param setter
+      * @tparam T
+      * @return
+      */
+    final def bind[T: TypeTag]( getter: => T, setter: T => Unit ): FXProperty[T] = bind( new Binding[T](getter, setter))
 
     private def bind[T]( binding: Binding[T]): FXProperty[T] = {
-        knownProperties += binding
+        knownProperties.add(binding)
         binding.rollback() // initialize model property
         binding.modelProperty
     }
 
+    /**
+      * Commits view model properties to the associated model
+      */
     final def commit(): Unit = knownProperties.foreach(_.commit())
+
+    /**
+      * Rolls back view model properties to the original state using associated model
+      */
     final def rollback(): Unit = knownProperties.foreach(_.rollback())
+
+    /**
+      * Checks if the view model state is dirty, i.e. there differences between view model properties and source properties
+      * @return true if view model is dirty
+      */
     final def isDirty: Boolean = knownProperties.exists(_.isDirty)
+
+    /**
+      * Checks the dirty state for provided property
+      * @param modelProperty
+      * @return true if modelProperty is dirty
+      */
     final def isDirty(modelProperty: FXProperty[_] ): Boolean = {
         knownProperties.find(_.modelProperty == modelProperty).exists(_.isDirty)
     }
 
+    /**
+      * Unbinds all the bindings for the view model
+      */
     final def unbind(): Unit = knownProperties.foreach(_.unbind())
 
 }
 
-trait Binding[T] {
+class Binding[T: TypeTag]( subjectGetter: => T, val subjectSetter: T => Unit ) {
 
-    val subjectProperty: FXProperty[T]
-    val modelProperty: FXProperty[T]
+    require( subjectGetter != null )
+    require( subjectSetter != null )
 
-    def commit(): Unit = if ( isDirty ) subjectProperty.set(modelProperty.get)
-    def rollback(): Unit = modelProperty.set(subjectProperty.get)
-    def isDirty: Boolean = modelProperty.get != subjectProperty.get
+    protected def subjectGet: T = subjectGetter
+    protected def subjectSet(value: T): Unit = subjectSetter(value)
+
+    val modelProperty: FXProperty[T] = Binding.createProperty[T]
+
+    def commit(): Unit = if ( isDirty ) subjectSet(modelProperty.get)
+    def rollback(): Unit = modelProperty.set(subjectGet)
+    def isDirty: Boolean = modelProperty.get != subjectGet
     def unbind(): Unit = modelProperty.unbind()
 }
 
-private case class FXPropertyBinding[T](subjectProperty: FXProperty[T]) extends Binding[T]{
-    require( subjectProperty != null )
-    override val modelProperty: FXProperty[T] = subjectProperty.getClass.newInstance().asInstanceOf[FXProperty[T]]
-}
 
-private class BeanBinding[T]( subjectGetter: => T, subjectSetter: T => Unit ) {
-    val subjectProperty: FXProperty[T] = new FXProperty[T] {
+private object Binding {
 
-    }
-}
-
-// Java bean property bindings
-private case class BeanPropertyBinding[T: TypeTag](bean: AnyRef, propName: String )
-    extends Binding[T]{
-
-    val subjectProperty: FXProperty[T] = typeOf[T] match {
-        case t if t =:= typeOf[Boolean] => JavaBeanBooleanPropertyBuilder.create().bean(bean).name(propName.toString).build().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[String]  => JavaBeanStringPropertyBuilder.create().bean(bean).name(propName.toString).build().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[Int]     => JavaBeanIntegerPropertyBuilder.create().bean(bean).name(propName.toString).build().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[Long]    => JavaBeanLongPropertyBuilder.create().bean(bean).name(propName.toString).build().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[Float]   => JavaBeanFloatPropertyBuilder.create().bean(bean).name(propName.toString).build().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[Double]  => JavaBeanDoublePropertyBuilder.create().bean(bean).name(propName.toString).build().asInstanceOf[FXProperty[T]]
-        case _                          => JavaBeanObjectPropertyBuilder.create().bean(bean).name(propName.toString).build().asInstanceOf[FXProperty[T]]
+    def asProperty[T: TypeTag](bean: AnyRef, propName: String): FXProperty[T] = {
+        ( typeOf[T] match {
+            case t if t =:= typeOf[Boolean] => JavaBeanBooleanPropertyBuilder.create().bean(bean).name(propName.toString).build()
+            case t if t =:= typeOf[String]  => JavaBeanStringPropertyBuilder.create().bean(bean).name(propName.toString).build()
+            case t if t =:= typeOf[Int]     => JavaBeanIntegerPropertyBuilder.create().bean(bean).name(propName.toString).build()
+            case t if t =:= typeOf[Long]    => JavaBeanLongPropertyBuilder.create().bean(bean).name(propName.toString).build()
+            case t if t =:= typeOf[Float]   => JavaBeanFloatPropertyBuilder.create().bean(bean).name(propName.toString).build()
+            case t if t =:= typeOf[Double]  => JavaBeanDoublePropertyBuilder.create().bean(bean).name(propName.toString).build()
+            case _                          => JavaBeanObjectPropertyBuilder.create().bean(bean).name(propName.toString).build()
+        }).asInstanceOf[FXProperty[T]]
     }
 
-    val modelProperty: FXProperty[T] = typeOf[T] match {
-        case t if t =:= typeOf[Boolean] => new SimpleBooleanProperty().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[String]  =>  new SimpleStringProperty().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[Int]     =>  new SimpleIntegerProperty().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[Long]    =>  new SimpleLongProperty().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[Float]   =>  new SimpleFloatProperty().asInstanceOf[FXProperty[T]]
-        case t if t =:= typeOf[Double]  =>  new SimpleDoubleProperty().asInstanceOf[FXProperty[T]]
-        case _                          =>  new SimpleObjectProperty[T]().asInstanceOf[FXProperty[T]]
+    def createProperty[T: TypeTag]: FXProperty[T] = {
+        (typeOf[T] match {
+            case t if t =:= typeOf[Boolean] => new SimpleBooleanProperty()
+            case t if t =:= typeOf[String] => new SimpleStringProperty()
+            case t if t =:= typeOf[Int] => new SimpleIntegerProperty()
+            case t if t =:= typeOf[Long] => new SimpleLongProperty()
+            case t if t =:= typeOf[Float] => new SimpleFloatProperty()
+            case t if t =:= typeOf[Double] => new SimpleDoubleProperty()
+            case _ => new SimpleObjectProperty[T]()
+        }).asInstanceOf[FXProperty[T]]
     }
 
 }
+
+import Binding._
+
+private class FXPropertyBinding[T: TypeTag](subjectProperty: FXProperty[T]) extends Binding[T](subjectProperty.get, subjectProperty.set )
+
+private class BeanPropertyBinding[T: TypeTag](bean: AnyRef, propName: String ) extends FXPropertyBinding[T]( asProperty(bean,propName))
+
 
